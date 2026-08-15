5294f2f8e868774761136077dfcc2dfe32f9bf73
Jorge <jorgew965@gmail.com>
2026-08-15 20:08:26 -0300

    add stdbool.h, add free

--- kernel/LibC/stdlib.c
+++ kernel/LibC/stdlib.c
@@ -1,4 +1,10 @@
 #include "stdlib.h"
+#include "stdbool.h"
+struct header {
+    size_t size;
+    bool livre;
+};
+
 extern char kernel_end;
 char* itoa(unsigned long num){
     char it[16] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
@@ -18,15 +24,23 @@ char* itoa(unsigned long num){
 static char* heap = (char*)&kernel_end;
 void* malloc(size_t size){
     void* endereco = heap;
-    heap += size;
+    heap += sizeof(struct header) + size;
     return endereco;
 }
 void* calloc(size_t nmemb, size_t size){
+    if(size == 0) return NULL;
     void* endereco = heap;
     heap += nmemb * size;
     char* bytes = (char*)endereco;
-    for(size_t i;i <= nmemb; i++){
+    for(size_t i=0;i < nmemb*size; i++){
         bytes[i] = 0;
-    }
+    } 
     return endereco;
+}
+void free(void* ptr){
+    if (ptr == NULL) {
+        return;
+    }
+    struct header* h = (struct header*)ptr - 1;
+    h->livre = 1;
 }
--- kernel/LibC/stdlib.h
+++ kernel/LibC/stdlib.h
@@ -1,12 +1,12 @@
 #ifndef STDLIB_H
 #define STDLIB_H
 typedef unsigned long long size_t;
+#define NULL ((void*)0)
 void* calloc(size_t nmemb, size_t size);
 void  free(void* ptr);
 void* malloc(size_t size);
 void* realloc(void* ptr, size_t size);
 char* itoa(unsigned long num);
-
 
 
 #endif
