ef21338555ef3090883f1329c1256230d17fa1eb
jwilliam-santos <jorgew965@gmail.com>
2026-09-03 21:46:32 -0300

    Rename LibC to libc

--- kernel/libc/apic.h
+++ kernel/libc/apic.h
@@ -7,10 +7,7 @@
 #include <stdlib.h>
 #define PIC1 0x20
 #define PIC2 0xA0
-#define PIC1_COMMAND PIC1
-#define PIC1_DATA (PIC1 + 1)
-#define PIC2_COMMAND PIC2
-#define PIC2_DATA (PIC2 + 1)
+
 void pic_disable(void);
 void Geral_apic(void);
 
