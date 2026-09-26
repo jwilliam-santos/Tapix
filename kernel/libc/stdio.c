c2b8aaa5b5f9d0ac3183d5e97beb2d4e0f333a2a
Ryuzaki-Nightmare <jorgew965@gmail.com>
2026-09-26 13:03:24 -0300

    Att stdio.h and add stdio.c

---
+++ kernel/libc/stdio.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <vga.h>
+/*
+* PRINTF TYPES
+* %d type = int
+* %f type = double 
+* %c types = char
+* %s type = String
+* %x Unsignet interger in hexadecimal (optional)
+* %p type = Pointer    
+*/
--- kernel/libc/stdio.h
+++ kernel/libc/stdio.h
@@ -1,4 +1,6 @@
 #ifndef STDIO_H
 #define STDIO_H
-
+int printf(const char* __restrict, ...);
+int putchar(int);
+int puts(const char*);
 #endif
